b6d01bdc0417b595e812963033daf4308b4f1b8c
TJKIM0219 <ktj9102@naver.com>
2024-11-16 15:31:38 +0900

    sh 파일수정

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,30 +1,9 @@
-```yaml
-version: 0.0
-os: linux
-files:
-  - source: /
-    destination: /home/ec2-user/app
-permissions:
-  - object: /home/ec2-user/app
-    pattern: "**"
-    owner: ec2-user
-    group: ec2-user
-    mode: 755
-hooks:
-  BeforeInstall:
-    - location: scripts/before_install.sh
-      timeout: 300
-      runas: ec2-user
-  ApplicationStart:
-    - location: scripts/start_application.sh
-      timeout: 300
-      runas: ec2-user
-  ApplicationStop:
-    - location: scripts/stop_application.sh
-      timeout: 300
-      runas: ec2-user
-  # ValidateService:
-  #   - location: scripts/validate_service.sh
-  #     timeout: 300
-  #     runas: ec2-user
-```
+#!/bin/bash
+if [ -d /home/ec2-user/app ]; then
+    rm -rf /home/ec2-user/app/*
+else
+    mkdir -p /home/ec2-user/app
+fi
+
+# 로그 디렉토리 생성
+mkdir -p /home/ec2-user/app/logs
